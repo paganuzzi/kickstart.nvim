618b7b3b4981c90ed7af6e2d1c66b39f76520e1b
Juan Paganuzzi <paganuzzi@gmail.com>
2024-03-20 22:42:18 -0300

    una mierda lo que necesita para funcionar

--- lua/juan/plugins/rest.lua
+++ lua/juan/plugins/rest.lua
@@ -1,58 +1,18 @@
 return {
-  "rest-nvim/rest.nvim",
-  dependencies = { { "nvim-lua/plenary.nvim" } },
-  ft = "http",
-  config = function()
-    require("rest-nvim").setup({
-      -- Open request results in a horizontal split
-      result_split_horizontal = false,
-      -- Keep the http file buffer above|left when split horizontal|vertical
-      result_split_in_place = false,
-      -- stay in current windows (.http file) or change to results window (default)
-      stay_in_current_window_after_split = true,
-      -- Skip SSL verification, useful for unknown certificates
-      skip_ssl_verification = false,
-      -- Encode URL before making request
-      encode_url = true,
-      -- Highlight request on run
-      highlight = {
-        enabled = true,
-        timeout = 150,
-      },
-      result = {
-        -- toggle showing URL, HTTP info, headers at top the of result window
-        show_url = true,
-        -- show the generated curl command in case you want to launch
-        -- the same request via the terminal (can be verbose)
-        show_curl_command = true,
-        show_http_info = true,
-        show_headers = true,
-        -- table of curl `--write-out` variables or false if disabled
-        -- for more granular control see Statistics Spec
-        show_statistics = false,
-        -- executables or functions for formatting response body [optional]
-        -- set them to false if you want to disable them
-        formatters = {
-          json = "jq",
-          html = function(body)
-            return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-          end,
-        },
-      },
-      -- Jump to request line on run
-      jump_to_request = false,
-      env_file = ".env",
-      -- for telescope select
-      env_pattern = "\\.env$",
-      env_edit_command = "tabedit",
-      custom_dynamic_variables = {},
-      yank_dry_run = true,
-      search_back = true,
-      --- Get the same options from Packer setup
-    })
-  end,
-  keys = {
-    { "<leader>rr", "<cmd>lua require('rest-nvim').run()<cr>", desc = "Run Rest Nvim" },
-    { "<leader>rl", "<cmd>lua require('rest-nvim').last()<cr>", desc = "Run Last Rest Nvim" },
+  'rest-nvim/rest.nvim',
+  lazy = false,
+  ft = 'http',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-treesitter/nvim-treesitter',
+    {
+      'vhyrro/luarocks.nvim',
+      config = function()
+        require('luarocks').setup { rocks = { 'lua-curl', 'xml2lua' } }
+      end,
+    },
   },
+  config = function()
+    require('rest-nvim').setup()
+  end,
 }
