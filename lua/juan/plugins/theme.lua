2133866c61464ba5b5fe26486ea9e8846378f4d2
Juan Paganuzzi <paganuzzi@gmail.com>
2024-03-29 05:24:32 -0300

    cambio tema

--- lua/juan/plugins/theme.lua
+++ lua/juan/plugins/theme.lua
@@ -1,10 +1,7 @@
 return {
-  'rose-pine/neovim',
-  event = 'VeryLazy',
+  'olimorris/onedarkpro.nvim',
   priority = 1000,
-  name = 'rose-pine',
   init = function()
-    vim.cmd 'colorscheme rose-pine'
+    vim.cmd 'colorscheme onedark_dark'
   end,
-  config = true,
 }
