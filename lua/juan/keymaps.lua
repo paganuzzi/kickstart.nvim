8da02000928de99e8b23421fa89e77d7622764af
Juan Paganuzzi <paganuzzi@gmail.com>
2024-04-06 19:38:31 -0300

    prueba clave

--- lua/juan/keymaps.lua
+++ lua/juan/keymaps.lua
@@ -2,7 +2,7 @@
 vim.keymap.set('n', '<leader>gs', '<cmd>:Gitsigns<cr>', { desc = 'GitSigns' })
 vim.keymap.set('n', '<leader>gr', '<cmd>:Gvdiffsplit!<cr>', { desc = 'GitDiffSplit' })
 
---bufers
+--Bufers
 vim.keymap.set('n', '<leader>ba', '<cmd>%bd<cr>', { desc = 'Borra todos los bufers' })
 vim.keymap.set('n', '<leader>bd', '<cmd>bd<cr>', { desc = 'Borra el buffer actual' })
 
