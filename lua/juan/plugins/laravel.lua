a0ab75fedf243ebc9f7d08bb2ce9a189d24c73f8
Juan Paganuzzi <paganuzzi@gmail.com>
2024-10-06 13:03:22 -0300

    changes in laravel plugin options

--- lua/juan/plugins/laravel.lua
+++ lua/juan/plugins/laravel.lua
@@ -13,5 +13,56 @@ return {
     { '<leader>fm', ':Laravel related<cr>', desc = 'Related' },
     { '<leader>ft', ':Laravel test:watch<cr>', desc = 'Test Watch' },
   },
+  opts = {
+    features = {
+      null_ls = {
+        enable = false,
+      },
+      route_info = {
+        enable = false, --- to enable the laravel.nvim virtual text
+        position = 'right', --- where to show the info (available options 'right', 'top')
+        middlewares = false, --- wheather to show the middlewares section in the info
+        method = false, --- wheather to show the method section in the info
+        uri = false, --- wheather to show the uri section in the info
+      },
+    },
+    ui = {
+      default = 'split',
+      nui_opts = {
+        split = {
+          enter = true,
+          relative = 'editor',
+          position = 'right',
+          size = '40%',
+          buf_options = {},
+          win_options = {
+            number = false,
+            relativenumber = false,
+          },
+        },
+        popup = {
+          enter = true,
+          focusable = true,
+          relative = 'editor',
+          border = {
+            style = 'rounded',
+          },
+          position = {
+            row = '20%',
+            col = '50%',
+          },
+          size = {
+            width = '28%',
+            height = '35%',
+          },
+          buf_options = {},
+          win_options = {
+            number = false,
+            relativenumber = false,
+          },
+        },
+      },
+    },
+  },
   config = true,
 }
