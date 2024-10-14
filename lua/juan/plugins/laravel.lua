f17da93498faa3787ac7964aaf5597356ba25a9a
Juan Paganuzzi <paganuzzi@gmail.com>
2024-10-14 19:46:13 -0300

    update laravel nvim

--- lua/juan/plugins/laravel.lua
+++ lua/juan/plugins/laravel.lua
@@ -4,14 +4,14 @@ return {
     'nvim-telescope/telescope.nvim',
     'tpope/vim-dotenv',
     'MunifTanjim/nui.nvim',
-    'nvimtools/none-ls.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'kevinhwang91/promise-async',
   },
-  cmd = { 'Sail', 'Artisan', 'Composer', 'Npm', 'Yarn', 'Laravel' },
+  cmd = { 'Laravel' },
   keys = {
     { '<leader>fa', ':Laravel artisan<cr>', desc = 'Artisan' },
     { '<leader>fr', ':Laravel routes<cr>', desc = 'Routes' },
     { '<leader>fm', ':Laravel related<cr>', desc = 'Related' },
-    { '<leader>ft', ':Laravel test:watch<cr>', desc = 'Test Watch' },
   },
   opts = {
     features = {
