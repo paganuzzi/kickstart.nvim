74da35d69cfd7bade86dbf04be0a528d453d6e91
Juan Paganuzzi <paganuzzi@gmail.com>
2024-12-06 23:29:47 -0300

    cambios en cmp y lsp

--- lua/juan/plugins/lsp.lua
+++ lua/juan/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return { -- LSP Configuration & Plugins
   'neovim/nvim-lspconfig',
-  event = 'BufRead',
+  event = { 'BufRead', 'BufEnter' },
   dependencies = {
     -- Automatically install LSPs and related tools to stdpath for neovim
     'williamboman/mason.nvim',
