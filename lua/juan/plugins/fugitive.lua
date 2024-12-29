1c2e5e6febbbee9803b3db7b82d72548d8bdb98d
Juan Paganuzzi <paganuzzi@gmail.com>
2024-12-29 14:25:29 -0300

    fugitive split vertical

--- lua/juan/plugins/fugitive.lua
+++ lua/juan/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
   -- enabled = false,
   cmd = { 'Git', 'G', 'Gvdiffsplit', 'Gllog' },
   keys = {
-    { '<leader>gf', ':Git<cr>', desc = 'Git Fugitive' },
+    { '<leader>gf', ':vertical Git<cr>', desc = 'Git Fugitive' },
     { '<leader>gp', ':Git push<cr>', desc = 'Git Fugitive Push' },
     { '<leader>gd', ':Gllog<cr>', desc = 'Git Logs' },
     { '<leader>gi', ':G add -i<cr>', desc = 'Git add interactive' },
