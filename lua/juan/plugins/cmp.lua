6ccebd42f174eb2c0a778529f16e4192db417379
Juan Paganuzzi <paganuzzi@gmail.com>
2025-05-09 20:49:55 -0300

    cambios

--- lua/juan/plugins/cmp.lua
+++ lua/juan/plugins/cmp.lua
@@ -1,24 +1,15 @@
 return {
   'saghen/blink.cmp',
   event = { 'BufRead' },
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    "folke/lazydev.nvim",
+  },
   version = '1.*',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
       preset = 'enter',
       ['<C-up>'] = { function(cmp)
@@ -28,20 +19,32 @@ return {
         cmp.scroll_documentation_down(4)
       end }
     },
-    signature = { enable = true },
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono'
     },
 
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = true } },
+
+    -- Disable cmdline
+    cmdline = { enabled = false },
+
+    completion = {
+      documentation = { auto_show = true, auto_show_delay_ms = 500 }
+    },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'cmdline', 'omni' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'cmdline', 'omni', 'lazydev' },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
+        },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
--- lua/juan/plugins/mason.lua
+++ lua/juan/plugins/mason.lua
@@ -1,9 +1,9 @@
-return{
+return {
     "mason-org/mason.nvim",
-    cmd={'Mason','MasonInstall','MasonUninstall'},
-    lazy=false,
-    config=true,
-    dependencies={
-     { 'j-hui/fidget.nvim', opts = {} } 
-  }
+    cmd = { 'Mason', 'MasonInstall', 'MasonUninstall' },
+    event = { 'BufRead' },
+    config = true,
+    dependencies = {
+        { 'j-hui/fidget.nvim', opts = {} }
+    }
 }
