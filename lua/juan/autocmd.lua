030f480b5d1fcb526b8ddbcab47a3f486f89ff96
Juan Paganuzzi <paganuzzi@gmail.com>
2025-05-15 00:11:17 -0300

    lspdetach

--- lua/juan/autocmd.lua
+++ lua/juan/autocmd.lua
@@ -80,9 +80,15 @@ vim.api.nvim_create_autocmd('LspAttach', {
 				callback = vim.lsp.buf.clear_references,
 			})
 		end
+		vim.api.nvim_create_autocmd('LspDetach', {
+			group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+			callback = function(event2)
+				vim.lsp.buf.clear_references()
+				vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+			end,
+		})
 	end,
 })
-
 
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking (copying) text',
